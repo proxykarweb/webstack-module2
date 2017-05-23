415e8d0f61763a5598c6d9b1b545d7bcb2b8fd70
proxykarweb <karchuenktang@gmail.com>
2017-05-23 01:09:14 -0400

    Add files via upload

---
+++ js/SpeakHello.js
@@ -0,0 +1,30 @@
+// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+// See Lecture 52, part 2
+
+
+// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
+// the "speak" method and which you will expose to the global context
+// See Lecture 52, part 1
+// var helloSpeaker =
+
+
+
+(function(window){
+	var helloSpeaker = {};
+	helloSpeaker.speak = function(name){
+			document.write("hello" + name); 
+		};
+
+	window.helloSpeaker = helloSpeaker;
+})(window);
+
+
+
+
+//helloSpeaker.speak("Name"); 
+
+// STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
+// 'helloSpeaker' on the global scope as well.
+// See Lecture 52, part 2
+// (Note, Step 6 will be done in the SpeakGoodBye.js file.)
+// xxxx.xxxx = helloSpeaker;
